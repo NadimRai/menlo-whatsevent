d15c0d2bb674a0924932f0a3015f072cd451df24
NadimRai <rai_nadim@yahoo.com>
2017-09-06 15:37:10 +0800

    created admin/attendances and comments controler and routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
     root "application#index"
     resources :users, only: [:index, :show]
     resources :categories
+    resources :comments, only: [:index]
+    resources :attendances, only: [:index]
   end
 
   devise_for :users
